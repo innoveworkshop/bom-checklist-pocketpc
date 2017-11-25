8dc88622c6457ddcf697c1737bfc57358cc68dc0
Nathan Campos <eeepc904@gmail.com>
2017-11-25 20:56:55 -0200

    Finished populating the detail view

--- main.pas
+++ main.pas
@@ -47,6 +47,7 @@ type
     procedure treeComponentsSelectionChanged(Sender: TObject);
   private
     procedure PopulateComponentTree;
+    procedure ShowDetail(id: Integer);
   public
     { public declarations }
   end;
@@ -103,9 +104,11 @@ end;
 
 procedure TMainForm.treeComponentsSelectionChanged(Sender: TObject);
 begin
+  grpComponentDetail.Enabled := true;
+
   if not treeComponents.Selected.HasChildren then
   begin
-    WriteLn(IntToStr(TTreeData(treeComponents.Selected.Data).ID));
+    ShowDetail(TTreeData(treeComponents.Selected.Data).ID);
   end;
 end;
 
@@ -117,9 +120,11 @@ var
   data: TTreeData;
 begin
   treeComponents.Items.Clear;
+  cmbCategory.Items.Clear;
 
   for i := 0 to BOM.Categories.Count - 1 do
   begin
+    cmbCategory.Items.Add(BOM.Categories.Strings[i]);
     node := treeComponents.Items.Add(nil, BOM.Categories.Strings[i]);
 
     for j := 0 to Length(BOM.Components) - 1 do
@@ -127,13 +132,45 @@ begin
       if BOM.Components[j].Category = BOM.Categories.Strings[i] then
       begin
         data := TTreeData.Create;
-        data.ID := BOM.Components[j].ID;
+        data.ID := j;
 
         treeComponents.Items.AddChildObject(node, BOM.Components[j].RefDes, data);
       end;
     end;
 
     node.Expand(true);
+  end;
+
+  statusBar.Panels.Items[0].Text := 'BOM file loaded';
+end;
+
+procedure TMainForm.ShowDetail(id: Integer);
+var
+  component: TComponent;
+  refs: TStringList;
+  i: Integer;
+begin
+  component := BOM.Components[id];
+  refs := TStringList.Create;
+
+  txtQuantity.Text := IntToStr(component.Quantity);
+  txtValue.Text := component.Value;
+  txtName.Text := component.Name;
+
+  for i := 0 to cmbCategory.Items.Count - 1 do
+  begin
+    if cmbCategory.Items[i] = component.Category then
+       cmbCategory.ItemIndex := i;
+  end;
+
+  refs.Clear;
+  refs.Delimiter := ',';
+  refs.StrictDelimiter := false;
+  refs.DelimitedText := component.RefDes;
+  lstRefDes.Items.Clear;
+  for i := 0 to refs.Count - 1 do
+  begin
+    lstRefDes.Items.Add(refs.Strings[i]);
   end;
 end;
 
